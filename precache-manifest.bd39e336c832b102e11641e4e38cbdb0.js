55c7ccfdf0d6801eb07221852d95852c6bbdaeef
SHARON ZHENG <sharzheng@gmail.com>
2019-08-09 23:16:44 -0400

    Updates

---
+++ precache-manifest.bd39e336c832b102e11641e4e38cbdb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21ede82fa336589e7a75eb8c203ae89",
+    "url": "/see-attle/index.html"
+  },
+  {
+    "revision": "bf4caac7e98153407d0c",
+    "url": "/see-attle/static/css/main.41d29b44.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/see-attle/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "bf4caac7e98153407d0c",
+    "url": "/see-attle/static/js/main.a542c0fc.chunk.js"
+  },
+  {
+    "revision": "5df30d38ecaea2bc58a3",
+    "url": "/see-attle/static/js/runtime~main.df09cfc7.js"
+  },
+  {
+    "revision": "d07535ed14e47dd879c110c15ee8da70",
+    "url": "/see-attle/static/media/Main.d07535ed.ttf"
+  },
+  {
+    "revision": "4ec67681acc5c49421ab298fd49a0382",
+    "url": "/see-attle/static/media/Wide.4ec67681.ttf"
+  },
+  {
+    "revision": "e677534c16f3f8e919766aafc7371f68",
+    "url": "/see-attle/static/media/test.e677534c.png"
+  }
+]);
